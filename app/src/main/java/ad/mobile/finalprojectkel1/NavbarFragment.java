ae08e604e7eda991f6954cb38378349f6cd9988c
aliefdany <aliefseventri@gmail.com>
2023-12-01 11:11:50 +0700

    integrated add mahasiswa layout to activity

--- app/src/main/java/ad/mobile/finalprojectkel1/NavbarFragment.java
+++ app/src/main/java/ad/mobile/finalprojectkel1/NavbarFragment.java
@@ -1,9 +1,13 @@
 package ad.mobile.finalprojectkel1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
@@ -12,6 +16,8 @@ public class NavbarFragment extends Fragment implements View.OnClickListener {
     private INavbar activity;
 
     private View layout;
+
+    private ImageButton btAddMhs;
 
 
     public NavbarFragment(INavbar activity){
@@ -24,11 +30,16 @@ public class NavbarFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         this.layout = inflater.inflate(R.layout.navigation_fragment, container, false);
 
+        this.btAddMhs = this.layout.findViewById(R.id.btAdd);
+
+        this.btAddMhs.setOnClickListener(this);
+
         return this.layout;
     }
 
     @Override
     public void onClick(View view) {
-
+        Intent i = new Intent((Context) this.activity,AddStudentActivity.class);
+        startActivity(i);
     }
 }
