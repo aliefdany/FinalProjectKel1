3e17b6c9883bb27d3953b8800192eb038f266055
aliefdany <aliefseventri@gmail.com>
2023-12-07 18:28:32 +0700

    updates

--- app/src/main/java/ad/mobile/finalprojectkel1/HomeFragment.java
+++ app/src/main/java/ad/mobile/finalprojectkel1/HomeFragment.java
@@ -1,26 +1,100 @@
 package ad.mobile.finalprojectkel1;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class HomeFragment extends Fragment {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HomeFragment extends Fragment implements ValueEventListener {
     private INavbar activity;
 
     private View layout;
 
 
+    private RecyclerView rvMahasiswa;
+    private List<Mahasiswa> listMahasiswa;
+    private MahasiswaAdapter adapterMahasiswa;
+    private EditText etCari;
+
+    private String url = "https://final-project-papb-de61c-default-rtdb.asia-southeast1.firebasedatabase.app/";
+    private DatabaseReference db;
+    private DatabaseReference appDb;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        this.db = FirebaseDatabase.getInstance(url).getReference();
+        this.appDb = this.db.child("mahasiswa");
+
         // Inflate the layout for this fragment
         this.layout = inflater.inflate(R.layout.home_fragment, container, false);
 
         return this.layout;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        listMahasiswa = new ArrayList<>();
+
+        this.appDb.addValueEventListener(this);
+    }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        this.listMahasiswa = new ArrayList<>();
+        // Iterate through the children and add them to the ArrayList
+        for (DataSnapshot snap : snapshot.getChildren()) {
+            Map<Mahasiswa, Object> map = (Map<Mahasiswa, Object>) snap.getValue();
+
+//            Log.d("TAG", "Value is: " + map);
+
+
+            listMahasiswa.add(new Mahasiswa(map.get("id").toString(),
+                    map.get("name").toString(),
+                    map.get("nim").toString(),
+                    map.get("prodi").toString(),
+                    map.get("fakultas").toString(),
+                    map.get("email").toString(),
+                    map.get("alamat").toString(),
+                    map.get("phoneNumber").toString()));
+        }
+
+        this.rvMahasiswa = this.layout.findViewById(R.id.rvMahasiswa);
+
+        this.adapterMahasiswa = new MahasiswaAdapter(this.layout.getContext());
+        this.adapterMahasiswa.setData(this.listMahasiswa);
+
+        this.rvMahasiswa.setLayoutManager(new LinearLayoutManager(this.layout.getContext()));
+
+        this.rvMahasiswa.setAdapter(this.adapterMahasiswa);
+
+        this.adapterMahasiswa.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
+
+    }
 }
---
+++ app/src/main/java/ad/mobile/finalprojectkel1/MahasiswaAdapter.java
@@ -0,0 +1,158 @@
+package ad.mobile.finalprojectkel1;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaAdapter.ItemVH> implements Filterable {
+    private final Context context;
+    private List<Mahasiswa> data;
+
+    private List<Mahasiswa> dataOriginal;
+
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+
+            @Override
+            protected FilterResults performFiltering(CharSequence search) {
+
+                String searchLower = search.toString().toLowerCase();
+
+                FilterResults results = new FilterResults();
+                ArrayList<Mahasiswa> filteredTweets = new ArrayList<>();
+
+                for(Mahasiswa c: dataOriginal) {
+                    if (c.getName().toLowerCase().contains(searchLower) || c.getNIM().toLowerCase().contains(searchLower)) {
+                        filteredTweets.add(c);
+                    }
+                }
+
+                results.values = filteredTweets;
+                results.count = filteredTweets.size();
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                data = (ArrayList<Mahasiswa>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+
+    }
+
+    @NonNull
+    @Override
+    public ItemVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View rowView = LayoutInflater.from(context).inflate(R.layout.item_mahasiswa, parent, false);
+
+        ItemVH viewHolder = new ItemVH(rowView);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemVH holder, int position) {
+
+        Mahasiswa k = this.data.get(position);
+
+        holder.tvUsername.setText(k.getName());
+        holder.tvKonten.setText(k.getNIM());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.data.size();
+    }
+
+    public MahasiswaAdapter(Context context) {
+        this.context = context;
+        this.data = new ArrayList<>();
+    }
+
+    public void setData(List<Mahasiswa> data) {
+        this.data = data;
+        this.dataOriginal = data;
+    }
+
+    class ItemVH extends RecyclerView.ViewHolder {
+
+        private TextView tvUsername;
+        private TextView tvKonten;
+
+        private ImageButton btDeleteTweet;
+
+
+        public ItemVH(@NonNull View itemView) {
+            super(itemView);
+
+            this.tvUsername = (TextView) itemView.findViewById(R.id.tvNamaMahasiswa);
+            this.tvKonten = (TextView) itemView.findViewById(R.id.tvNIM);
+            this.btDeleteTweet = (ImageButton) itemView.findViewById(R.id.btDeleteMahasiswa);
+
+
+            btDeleteTweet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showDeleteDialog();
+                }
+            });
+        }
+
+        void showDeleteDialog() {
+            final Dialog dialog = new Dialog(context);
+
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setCancelable(true);
+            dialog.setContentView(R.layout.delete_mahasiswa_dialog);
+
+//            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+            Button btConfirmDeleteTweet = (Button) dialog.findViewById(R.id.btConfirmDeleteTweet);
+
+            btConfirmDeleteTweet.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Log.d("Removing tweet with index", String.valueOf(getAdapterPosition()));
+
+                    removeAt(getAdapterPosition());
+
+                    dialog.dismiss();
+
+                }
+            });
+
+            dialog.show();
+        }
+
+        public void removeAt(int position) {
+            data.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, data.size());
+        }
+
+    }
+}
