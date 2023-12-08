a2e0e5e012cae72c5167ee683525c9a2650634d5
aliefdany <aliefseventri@gmail.com>
2023-12-08 21:33:28 +0700

    added register page

---
+++ app/src/main/java/ad/mobile/finalprojectkel1/EmailPasswordActivity.java
@@ -0,0 +1,34 @@
+package ad.mobile.finalprojectkel1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class EmailPasswordActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_password);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
+
+
+    }
+}
