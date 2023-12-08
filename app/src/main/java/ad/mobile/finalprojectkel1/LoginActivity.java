2b3841d6174a51137bdc787852005450ff4d393e
aliefdany <aliefseventri@gmail.com>
2023-12-08 21:48:50 +0700

    linked daftar and masuk page

--- app/src/main/java/ad/mobile/finalprojectkel1/LoginActivity.java
+++ app/src/main/java/ad/mobile/finalprojectkel1/LoginActivity.java
@@ -3,12 +3,25 @@ package ad.mobile.finalprojectkel1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    private TextView tvDaftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.tvDaftar = findViewById(R.id.tvDaftar);
+
+        this.tvDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/ad/mobile/finalprojectkel1/RegisterActivity.java
+++ app/src/main/java/ad/mobile/finalprojectkel1/RegisterActivity.java
@@ -2,7 +2,11 @@ package ad.mobile.finalprojectkel1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,10 +15,25 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     private FirebaseAuth mAuth;
+
+    private TextView loginTextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.loginTextButton = findViewById(R.id.tvMasuk);
+
+
+        Context ctx = this;
+        this.loginTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ctx, LoginActivity.class);
+                startActivity(i);
+            }
+        });
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
