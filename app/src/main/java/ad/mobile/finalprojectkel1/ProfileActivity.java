38534e907516f9e6f86be8a509d7f78c63ef6833
aliefdany <aliefseventri@gmail.com>
2023-12-12 16:26:14 +0700

    addedn log out dialog

--- app/src/main/java/ad/mobile/finalprojectkel1/ProfileActivity.java
+++ app/src/main/java/ad/mobile/finalprojectkel1/ProfileActivity.java
@@ -1,14 +1,63 @@
 package ad.mobile.finalprojectkel1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private Button btLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        this.btLogout = findViewById(R.id.btLogout);
+        this.btLogout.setOnClickListener(this);
+
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+
+    void showLogoutDialog() {
+        final Dialog dialog = new Dialog(this);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(true);
+        dialog.setContentView(R.layout.logout_dialog);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+//            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        Button btConfirmLogout = (Button) dialog.findViewById(R.id.btConfirmLogout);
+
+
+        btConfirmLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+
+                dialog.dismiss();
+
+            }
+        });
+
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        showLogoutDialog();
     }
 }
